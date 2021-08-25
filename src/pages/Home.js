a6f902b91740146f36c4753e177ed1cb46dc1930
Colin Edwards <ColinJhEdwards@gmail.com>
2021-08-24 20:38:21 -0700

    removing imports that were not used

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { loadItemDetail } from "../actions/itemAction";
 import Chart from "../components/Chart";
